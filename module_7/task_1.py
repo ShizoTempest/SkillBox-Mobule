aa8e38ea2abf8ee41dd5361b78715a22ab7f359d
ShizoTempest <tisarutempest@mail.ru>
2021-04-30 20:41:40 +0300

    task1 done

--- module_7/task_1.py
+++ module_7/task_1.py
@@ -1,1 +1,5 @@
-print('Hello')
+for numbers in 114, 12, 14, 10605, 4907, 450:
+    if numbers % 2 == 0 and numbers % 3 != 0:
+        print('Число подоходит', numbers)
+    else:
+        print('Число не подоходит', numbers)
