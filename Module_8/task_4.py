f8487d8b17c121292ce72c3e1bde728287bcd378
ShizoTempest <tisarutempest@mail.ru>
2021-06-04 14:52:59 +0300

    Module 8 done. Please, correct and repair code

--- Module_8/task_4.py
+++ Module_8/task_4.py
@@ -2,9 +2,13 @@ a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 с = int(input('Введите кратность: '))
 summ = 0
+count = 0
+if a > b:
+  a, b = b, a
 for num in range(a, b):
-    if num // 3 == 0:
-        summ += 1
+    if num % с == 0:
+        summ += num
+        count += 1
 print('Общее кол-во кратностей:', summ)
 
 #Решение неверное.
