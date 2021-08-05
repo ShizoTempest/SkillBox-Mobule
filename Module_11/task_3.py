b6345a5a6313bab5ed47c3ac6e30c7c3e3677b36
ShizoTempest <tisarutempest@mail.ru>
2021-08-06 00:26:31 +0300

    Я работал, а он hajota. Я чинил, а он sotkea. Я востанавливал, а он lensi ulos. Я просыпался, а он Hän lähetti kolme kirjettä.

--- Module_11/task_3.py
+++ Module_11/task_3.py
@@ -1,7 +1,7 @@
 exept = float(input('Укажите размер файла: '))
 speed = float(input('Какова скорость загрузки: '))
 end = 0
-second = 1
+second = 0
 while end <= exept:
     print('Прошло', second, 'сек. Скачано', end, 'из', exept, 'МБ "', round(end / exept * 100, 0) ,'% ".')
     end += speed
