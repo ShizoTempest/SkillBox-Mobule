1f1685fc68ed0c896c613be7751f3d0ae39ad2c2
ShizoTempest <tisarutempest@mail.ru>
2021-08-29 01:54:31 +0300

    先輩, это было долгий путь...

--- Module_12/task_8.py
+++ Module_12/task_8.py
@@ -1,1 +1,23 @@
-# todo здесь писать код
+def NOD():
+    cycle = True
+    while cycle:
+        first = int(input('Введите первое число: '))
+        second = int(input('Введите второе число: '))
+        if first < second:
+            first, second = second, first
+            cycle = False
+        elif first == second:
+            print('Ошибочка. Числа должны быть разными.')
+        elif first > second:
+            cycle = False
+    cycle = True
+    while cycle:
+        num = second
+        second = first % second
+        first = num
+        if second == 0:
+            print('НОД этих чисел:', num)
+            break
+        print(num, first, second)
+
+NOD()
