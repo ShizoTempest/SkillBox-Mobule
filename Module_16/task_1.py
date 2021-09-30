cb9797d833db3bfe09ae4525b36669efd5c3c9c0
ShizoTempest <tisarutempest@mail.ru>
2021-09-30 18:55:05 +0300

    主なことは、それが行われることです

--- Module_16/task_1.py
+++ Module_16/task_1.py
@@ -1,19 +1,14 @@
-def mini(list_num, number):
-    t = 0
-    for i in list_num:
-        if i == number:
-            t += 1
-    print(f'Кол-во цифр {number} при первом объединении:', t)
-    return t
 a = [1, 5, 3]
 b = [1, 5, 1, 5]
 c = [1, 3, 1, 5, 3, 3]
 a.extend(b)
-t = mini(a, 5)
+t = a.count(5)
+print(f'Кол-во цифр {t} при первом объединении:', t)
 for _ in range(t):
     a.remove(5)
 a.extend(c)
-t = mini(a, 3)
+t = a.count(3)
+print(f'Кол-во цифр {t} при втором объединении:', t)
 print('Итоговый список:', a)
 
 
