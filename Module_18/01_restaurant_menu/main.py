8e68acd7d01b0ad6fbf8a5b90efc5fc2822cae5c
ShizoTempest <tisarutempest@mail.ru>
2021-12-20 13:55:46 +0300

    И-и та-ак сойдет

--- Module_18/01_restaurant_menu/main.py
+++ Module_18/01_restaurant_menu/main.py
@@ -1,1 +1,10 @@
-
+menu = input('Доступное меню: ')
+list_menu = menu.split(';')
+answer_menu = ''
+len_menu = len(list_menu)
+for word in list_menu:
+    if word == list_menu[len_menu - 1]:
+        answer_menu += word
+        continue
+    answer_menu += word + ', '
+print('На данный момент в меню есть:', answer_menu)
