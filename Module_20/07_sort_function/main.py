d0f7a4a1cd6a8adb005bf465f816c00152ae4e70
ShizoTempest <tisarutempest@mail.ru>
2022-03-20 20:39:44 +0300

    Repair 7 and 10 task

--- Module_20/07_sort_function/main.py
+++ Module_20/07_sort_function/main.py
@@ -1,16 +1,9 @@
 def tpl_sort(key):
-    list_num = list(key)
-    for num in list_num:
-        if str(num).count('.') >= 1:
-            return ()
-        else:
-            list_num.sort()
-            list_num = tuple(list_num)
-            return list_num
+    if all(key):
+        key = sorted(key)
+        list_num = tuple(key)
+        return list_num
+    else:
+        return ()
 
 print(tpl_sort((6, 3, -1, 8.5, 4, 10, -5)))
-
-# TODO не работает условие для дробных чисел (кортеж все равно сортируется). Почитай про функцию sorted() и не переводи
-#  без нужды все в список.
-#  Чтобы не использовать цикл для проверки каждого значения почитай про функцию all()
-#  4 и 5 строки - не принимаются здесь. Используй либо type(), либо isinstance()
