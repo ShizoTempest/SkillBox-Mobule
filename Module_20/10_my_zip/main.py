6a51b7adfacb0d5b1af98fad9c8ae7370f5f6b31
ShizoTempest <tisarutempest@mail.ru>
2022-03-21 14:32:24 +0300

    Repair 7 and 10 task

--- Module_20/10_my_zip/main.py
+++ Module_20/10_my_zip/main.py
@@ -1,22 +1,15 @@
-def input_user():
-    while True:
-        word = input('Введите строку: ')
-        word = list(word)
-        print('Пример: (10, 20, 30, 40)')
-        num_tuple = input('Кортеж чисел: ')
-        num_tuple = num_tuple[1:-1]
-        num_tuple = list(num_tuple.split(', '))
-        if len(word) != len(num_tuple):
-            print('Длина строки и кортежа чисел не равна.')
-        else:
-            break
-    return word, num_tuple
+def input_user(words, symbol):
+    return min(len(words), len(symbol))
 
-word, num_tuple = input_user()
+word = input('Введите строку: ')
+print('Пример: (10, 20, 30, 40)')
+num_tuple = input('Кортеж чисел: ')
+num_tuple = num_tuple[1:-1]
+num_tuple = tuple(num_tuple.split(', '))
 
-new_zip = zip(word, num_tuple)
-print(new_zip)
-new_zip = list(new_zip)
+pairs = ((word[counter], num_tuple[counter])
+                   for counter in range(input_user(word, num_tuple)))
+print(pairs)
 
-for elem in range(len(word)):
-    print(new_zip[elem])
+for element in pairs:
+    print(element)
